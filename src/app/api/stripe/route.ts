656002339549a78a258c5e71d9d59a9995390138
Rupesh sharma <rsharma@Rk-Air.local>
2024-10-18 17:36:04 +0530

    added log 2

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -62,6 +62,7 @@ export async function GET() {
     return NextResponse.json({ url: stripeSession.url });
   } catch (error) {
     console.log("stripe error", error);
+    console.log(return_url);
     console.log("error is here");
     return new NextResponse("internal server error", { status: 500 });
   }
